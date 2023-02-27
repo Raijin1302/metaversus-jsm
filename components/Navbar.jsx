c18b8f13699c06c9261d4f8382f9950ac981f8a4
Raijin1302 <raijinnguyen1302@gmail.com>
2023-02-27 23:31:59 +0700

    :green_cirlce: import utils motion

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,7 @@
-'use client';
-
-const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
-);
+"use client";
+import styles from "@/styles";
+import { navVariants } from "@/utils/motion";
+import { motion } from "framer-motion";
+const Navbar = () => <nav>navbar</nav>;
 
 export default Navbar;
