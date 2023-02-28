11660b805ddb048bf4c25eacaa64d596631a922c
Raijin1302 <raijinnguyen1302@gmail.com>
2023-03-01 00:51:01 +0700

    done layout NavBar on screen , not for mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
-import styles from "@/styles";
-import { navVariants } from "@/utils/motion";
+
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
 import { motion } from "framer-motion";
 const Navbar = () => (
   <motion.nav
@@ -8,8 +9,26 @@ const Navbar = () => (
     initial="hidden"
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
+
+    // sm:px-16 px-6 py-8
   >
-    navbar
+    NavBar
+    <div className="absolute w-1/2 inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="./search.svg"
+        alt=""
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+        METAVERSUS
+      </h2>
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
+    </div>
   </motion.nav>
 );
 
