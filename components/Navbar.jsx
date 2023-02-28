f4cb794672bf51ea712a84597494529e9945cb0b
Raijin1302 <raijinnguyen1302@gmail.com>
2023-02-28 23:57:27 +0700

    :red_circle: testing navbar motion

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,15 @@
 import styles from "@/styles";
 import { navVariants } from "@/utils/motion";
 import { motion } from "framer-motion";
-const Navbar = () => <nav>navbar</nav>;
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    navbar
+  </motion.nav>
+);
 
 export default Navbar;
