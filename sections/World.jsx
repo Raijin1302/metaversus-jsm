411dab70ada7055050c2ad38c6a1439071757eac
Raijin1302 <raijinnguyen1302@gmail.com>
2023-03-09 23:47:00 +0700

    :green_circle: done World section

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,9 +1,52 @@
-'use client';
+"use client";
+import { TitleText, TypingText } from "@/components";
+import styles from "@/styles";
+import { motion } from "framer-motion";
+import React from "react";
+import { staggerContainer, fadeIn } from "@/utils/motion";
 
-const World = () => (
-  <section>
-    World section
-  </section>
-);
+const World = () => {
+  return (
+    <section className={`${styles.paddings} relative z-10`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: "false", amount: 0.25 }}
+        className={`flex flex-col gap-8 mx-auto ${styles.innerWidth}`}
+      >
+        <TypingText title="| People on the world" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Track friends around you and invite them to play together in the
+              same world
+            </>
+          }
+          textStyles="text-center"
+        />
+      </motion.div>
+      <motion.div
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className="relative mt-16 flex w-full h-[550px]"
+      >
+        <img
+          src="./map.png"
+          alt="map"
+          className="w-full h-full object-contain"
+        />
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-2 rounded-full bg-[#5d6680]">
+          <img src="./people-01.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-2 rounded-full bg-[#5d6680]">
+          <img src="./people-02.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-2 rounded-full bg-[#5d6680]">
+          <img src="./people-03.png" alt="people" className="w-full h-full" />
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default World;
